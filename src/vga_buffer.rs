cf23f1beb48bcb91d22de84665cc9464075c10f3
KingZJ <1012714893@qq.com>
2023-03-14 22:51:43 +0800

    cargo fmt

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -137,7 +137,7 @@ lazy_static! {
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => {
-        $crate::vga_buffer::_print(format_args!($($arg)*));
+        $crate::vga_buffer::_print(format_args!($($arg)*))
     };
 }
 
